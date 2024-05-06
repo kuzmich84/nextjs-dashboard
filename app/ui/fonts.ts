8d877d2eea0c723099ca364c8d85a75427da16bc
Dmitry Kuchukov <kudim@inbox.ru>
2024-05-06 18:08:58 +0300

    init: init app

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter, Lusitana, Roboto } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
+
+export const roboto = Roboto({ subsets: ['cyrillic'], weight: ['400', '700'] });
